b0dc4e3176ab58d7e12bc9c4fc02f4ed26688c8a
Rune Frohn <rune@frohn.cologne>
2020-03-21 07:32:14 +0100

    add scraper for german Bundeszentrale fuer gesundheitliche Aufklaerung

---
+++ data/scrapers/BZgA_scraper.py
@@ -0,0 +1,64 @@
+# run 'scrapy runspider WHO_scraper.py' to scrape data
+
+from datetime import date
+import scrapy
+import pandas as pd
+
+
+class WHOScraper(scrapy.Spider):
+  name = "BZgA_scraper"
+  start_urls = ["https://www.infektionsschutz.de/coronavirus/faqs-coronaviruscovid-19.html"]
+
+  def parse(self, response):
+    columns = {
+      "question" : [],
+      "answer" : [],
+      "answer_html" : [],
+      "link" : [],
+      "name" : [],
+      "source" : [],
+      "category" : [],
+      "country" : [],
+      "region" : [],
+      "city" : [],
+      "lang" : [],
+      "last_update" : [],
+    }
+
+    QUESTION_ANSWER_SELECTOR = ".c-accordion__item"
+    QUESTION_SELECTOR = ".c-accordion__button::text"
+    ANSWER_SELECTOR = ".c-accordion__section ::text"
+    ANSWER_HTML_SELECTOR = ".c-text"
+
+    questions_answers = response.css(QUESTION_ANSWER_SELECTOR)
+    for question_answer in questions_answers:
+      question = question_answer.css(QUESTION_SELECTOR).getall()
+      question = " ".join(question).strip()
+      answer = question_answer.css(ANSWER_SELECTOR).getall()
+      answer = "".join(answer).strip()
+      answer_html = question_answer.css(ANSWER_HTML_SELECTOR).getall()
+      answer_html = " ".join(answer_html).strip()
+
+      # add question-answer pair to data dictionary
+      columns["question"].append(question)
+      columns["answer"].append(answer)
+      columns["answer_html"].append(answer_html)
+
+    today = date.today()
+
+    columns["link"] = ["https://www.infektionsschutz.de/coronavirus/faqs-coronaviruscovid-19.html"] * len(columns["question"])
+    columns["name"] = ["FAQs Coronavirus/Covid-19"] * len(columns["question"])
+    columns["source"] = ["Bundeszentrale für gesundheitliche Aufklärung"] * len(columns["question"])
+    columns["category"] = [""] * len(columns["question"])
+    columns["country"] = [""] * len(columns["question"])
+    columns["region"] = [""] * len(columns["question"])
+    columns["city"] = [""] * len(columns["question"])
+    columns["lang"] = ["de"] * len(columns["question"])
+    columns["last_update"] = [today.strftime("%Y/%m/%d")] * len(columns["question"])
+
+    dataframe = pd.DataFrame(columns)
+
+    dataframe.to_csv("bzga.tsv", sep="\t", index=False)
+ 
+
+
