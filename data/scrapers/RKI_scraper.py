d3787de69abed3fc4c12d36e1004f3e66bda7f46
Timo Moeller <timo.moeller@deepset.ai>
2020-03-21 10:58:05 +0100

    Adjust RKI scraper

--- data/scrapers/RKI_scraper.py
+++ data/scrapers/RKI_scraper.py
@@ -4,6 +4,7 @@ import scrapy
 import numpy as np
 from datetime import date
 import pandas as pd
+from scrapy.crawler import CrawlerProcess
 
 class rki_infos(scrapy.Spider):
 	name = 'rki_spyder'
@@ -26,7 +27,7 @@ class rki_infos(scrapy.Spider):
 			}
 
 		for x in response.xpath('//div[@class="alt-accordion-box-box"]/@id').extract():
-			question_text = response.xpath(str('//*[@id="'+x+'"]/h2/text()')).extract()
+			question_text = response.xpath(str('//*[@id="'+x+'"]/h2/text()')).extract()[0]
 			answer_text = " ".join(response.xpath(str('//*[@id="'+x+'"]/div/p')).xpath('string()').extract())
 
 
@@ -50,6 +51,12 @@ class rki_infos(scrapy.Spider):
 		dataframe.to_csv("rki.tsv", sep="\t", index=False)
 
 
+if __name__ == "__main__":
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
 
+    process.crawl(rki_infos)
+    process.start()
 
 
