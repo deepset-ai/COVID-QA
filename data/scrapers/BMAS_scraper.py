b7a80167e2ffaf3e0b7eab3f725a7c0b75811a3e
Timo Moeller <timo.moeller@deepset.ai>
2020-03-21 12:46:11 +0100

    test bmas

--- data/scrapers/BMAS_scraper.py
+++ data/scrapers/BMAS_scraper.py
@@ -1,6 +1,7 @@
 from datetime import date
 import scrapy
 import pandas as pd
+from scrapy.crawler import CrawlerProcess
 
 
 class CovidScraper(scrapy.Spider):
@@ -57,3 +58,11 @@ class CovidScraper(scrapy.Spider):
     dataframe = pd.DataFrame(columns)
 
     dataframe.to_csv("bmas_de.tsv", sep="\t", index=False)
+
+if __name__ == "__main__":
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    process.crawl(CovidScraper)
+    process.start()
