519139365f4c2134d635bc025643ce0e4eb50e0d
SarruSharma <63207249+SarruSharma@users.noreply.github.com>
2021-03-20 21:22:46 -0400

    Update autocomplete.py

--- backend/controller/autocomplete.py
+++ backend/controller/autocomplete.py
@@ -11,62 +11,62 @@ langid.set_languages(['de', 'en'])  # ISO 639-1 codes
 #
 from backend import api
 
-DB_INDEX_AUTOCOMPLETE = "autocomplete"
-
-router = APIRouter()
-
 
 class Request(BaseModel):
     search: str
 
 
-def addQuestionToAutocomplete(question: str):
-    # todo: if it already exists; we need to increment count;
-    body = {
-        'phrase': question,
-        'count' : 1
-    }
-    res = api.elasticsearch_client.index(index=DB_INDEX_AUTOCOMPLETE,body=body)
+class Question:
+    #Aggregate root
+    def __init__(self, question: str, search: str):
+        self.DB_INDEX_AUTOCOMPLETE = "autocomplete"
+        self.router = APIRouter()
+        self.question = question
+        self.search = search
 
-
-
-
-@router.get("/query/autocomplete")
-def ask(search: str):
-    interim = api.elasticsearch_client.search(index=DB_INDEX_AUTOCOMPLETE, body=
-    {
-        '_source':['phrase'],
-        'query':{
-            "bool": {
-                "must": [{
-                    "match": {
-                        "phrase": search
-                    }
-                },
-                    {
-                        "exists": {
-                            "field": "count"
-                        }
-                    }]
-            }
-        },
-        'size': 10,
-        'sort' :[
-                {'count' : {'order' : 'desc' }}
-        ]
-    })
-
-    resultCount = len(interim['hits']['hits'])
-    result = []
-    for i in range(resultCount):
-        result.append(interim['hits']['hits'][i]['_source']['phrase'])
-
-
-    lang, score = langid.classify(search)
-
-    return {
-            "results":result,
-            "language": lang
+    def addQuestionToAutocomplete(question: str):
+        # todo: if it already exists; we need to increment count;
+        body = {
+            'phrase': question,
+            'count' : 1
         }
+        res = api.elasticsearch_client.index(index=self.DB_INDEX_AUTOCOMPLETE,body=body)
 
 
+    @self.router.get("/query/autocomplete")
+    def ask(search: str):
+        interim = api.elasticsearch_client.search(index=self.DB_INDEX_AUTOCOMPLETE, body=
+        {
+            '_source':['phrase'],
+            'query':{
+                "bool": {
+                    "must": [{
+                        "match": {
+                            "phrase": search
+                        }
+                    },
+                        {
+                            "exists": {
+                                "field": "count"
+                            }
+                        }]
+                }
+            },
+            'size': 10,
+            'sort' :[
+                    {'count' : {'order' : 'desc' }}
+            ]
+        })
+
+        resultCount = len(interim['hits']['hits'])
+        result = []
+        for i in range(resultCount):
+            result.append(interim['hits']['hits'][i]['_source']['phrase'])
+
+
+        lang, score = langid.classify(search)
+
+        return {
+                "results":result,
+                "language": lang
+        }
