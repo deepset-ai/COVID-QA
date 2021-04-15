3a18e7840585e8106a5d8612761b6890c11c24f0
SwithinTan <swithintan@gmail.com>
2021-04-16 01:45:11 +0800

    PR2_1

--- covid_nlp/language/detect_language.py
+++ covid_nlp/language/detect_language.py
@@ -5,21 +5,62 @@ import hmac
 from hashlib import sha1
 from time import time
 import requests
+from abc import ABCMeta, abstractmethod
 
-class LanguageDetector():
-    def __init__(self, model = 'sil'):
+# apply bridge pattern
+# print method is commonly used by the three class
+
+# abstract class
+class Bridge:
+    def __init__(self, model):
+        self.model = model
+    def bridge_print(self):
+        pass
+
+# concrete abstract class
+class Bridge_cld2(Bridge):
+    def __init__(self, model):
         self.model = model
 
-    def detect_lang_cld2(self, text):
-        pred = cld2.detect(text)[2][0]
-        return pred[1], float(pred[2])
+class Bridge_cld3(Bridge):
+    def __init__(self, model):
+        self.model = model
 
-    def detect_lang_cld3(self, text):
+class Bridge_cil(Bridge):
+    def __init__(self, model):
+        self.model = model
+
+
+# Implementation class
+class DlPrint:
+    def Dl_print(self, text):
+        pass
+
+# concrete implementation class
+class DlPrint_Detect_lang_cld2(DlPrint):
+        
+    def Dl_print(self, text):
+        pred = cld2.detect(text)[2][0]
+        print(f"cld2: {pred[1], float(pred[2])}")
+
+
+class DlPrint_Detect_lang_cld3(DlPrint):
+        
+    def Dl_print(self, text, n = 3):
+        # print ld3_detect_result
         import cld3  # requires protobuf
         pred = cld3.get_language(text)
-        return pred.language, 100*pred.probability
+        print(f"cld3: {pred.language, 100*pred.probability}")
 
-    def detect_lang_sil(self, text):
+        # print ld3_freq_top_results
+        pred = cld3.get_frequent_languages(text, num_langs = n)
+        pred_list = [ (p.language, 100*p.probability) for p in pred ]
+        print(f"cld3-freq: {pred_list}")
+
+
+class DlPrint_Detect_lang_sil(DlPrint):
+
+    def Dl_print(self, text):
         algorithm = 'HMAC+SHA1'
         curr_time = str(int(time()))
         concat = curr_time+os.environ.get('SIL_API_KEY')
@@ -31,39 +72,23 @@ class LanguageDetector():
         headers = {'Content-Type': 'application/json'}
         r = requests.post(os.environ.get('SIL_API_URL'), json=[{"text": text}], 
                 headers=headers, params=params)
-        return r.json()[0]['language'], 100*r.json()[0]['probability']
-
-    def detect_lang(self, text):
-        if self.model == 'cld2':
-            return self.detect_lang_cld2(text)
-        if self.model == 'cld3':
-            return self.detect_lang_cld3(text)
-        if self.model == 'sil':
-            return self.detect_lang_sil(text)
-
-    def detect_freq_lang(self, text, n = 3):
-        import cld3  # requires protobuf
-        pred = cld3.get_frequent_languages(text, num_langs = n)
-        pred_list = [ (p.language, 100*p.probability) for p in pred ]
-        return pred_list
+        pred_sil = r.json()[0]['language'], 100*r.json()[0]['probability']
+        print(f"sil: {pred_sil}")
 
 
 def main():
     my_text = "Was ist das Coronavirus?"
+    ld3 = Bridge_cld3('ld3')
+    ld3_p = DlPrint_Detect_lang_cld3(text = my_text)
+    ld3_p.Dl_print
 
-    ld3 = LanguageDetector(model = 'cld3')
-    ld3_result = ld3.detect_lang(my_text)
-    print(f"cld3: {ld3_result}")
-    ld3_top_results = ld3.detect_freq_lang(my_text, 4)
-    print(f"cld3-freq: {ld3_top_results}")
+    ld2 = Bridge_cld2('ld2')
+    ld2_p = DlPrint_Detect_lang_cld2(text = my_text)
+    ld2_p.Dl_print
 
-    ld2 = LanguageDetector(model = 'cld2')
-    ld2_result = ld2.detect_lang(my_text)
-    print(f"cld2: {ld2_result}")
-
-    ldsil = LanguageDetector(model = 'sil')
-    ldsil_result = ldsil.detect_lang(my_text)
-    print(f"sil: {ldsil_result}")
+    ldsil = Bridge_cil('sil')
+    ldsil_p = DlPrint_Detect_lang_sil(text = my_text)
+    ldsil_p.Dl_print
 
 if __name__ == "__main__":
     main()
