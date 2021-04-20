c1deaceb7d3fbc982101b1b48e42846990dca775
SwithinTan <swithintan@gmail.com>
2021-04-21 02:16:44 +0800

    PR4_Behavioral_2

--- covid_nlp/modeling/tfidf/tfidf_client.py
+++ covid_nlp/modeling/tfidf/tfidf_client.py
@@ -36,24 +36,3 @@ class TfidfEvaluator():
         vec2 = self.process_string(string2)
         cos_sim = cosine_similarity(vec1, vec2)
         return cos_sim[0][0]
-
-def main():
-    evaluator = TfidfEvaluator()
-
-    eval_file = "../../../data/eval_question_similarity_en.csv"
-    df = pd.read_csv(eval_file)
-    # predict similarity of samples (e.g. via embeddings + cosine similarity)
-    df['pred'] = df.apply(lambda x: evaluator.score_string_pair(x.question_1, x.question_2), axis=1)
-    y_true = df["similar"].values
-    y_pred = df["pred"].values
-
-    model_name = "tfidf_baseline"
-    exp_name = "tfidf_cos_sim_2"
-    params = {"sp_voc": 16000, "max_ngram": 2, "remove_stopwords": 1, 
-                "data_train": "eval, scraped", "data_sp": "eval, scraped, CORD-19.200k"}
-    eval_question_similarity(y_true=y_true, y_pred=y_pred, lang="en", model_name=model_name,
-                             params=params, user="carmen", log_to_mlflow=True, run_name=exp_name)
-
-
-if __name__ == "__main__":
-    main()
